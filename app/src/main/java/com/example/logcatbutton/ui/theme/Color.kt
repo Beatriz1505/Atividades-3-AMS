b32c7bab08b5b98edb467af11a5eef04bfc517c7
Beatriz1505 <galdinob596@gmail.com>
2026-03-28 11:37:29 -0300

    Adicionando Trabalho

---
+++ app/src/main/java/com/example/logcatbutton/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.logcatbutton.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val LightGreen = Color(0xFF73E595)
+val Light = Color(0xFFBFC2C0)
+
+val LightOrange = Color(0xFFE0AD7A)
+val DarkRed = Color(0xFFB71C1C)
